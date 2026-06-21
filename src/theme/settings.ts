5e4c793316c4ee9ded3fa1588d41a71049b0c39f
yjr <yjr@YJRMacBook-Pro-5.local>
2026-06-21 20:04:42 +0800

    调整左侧菜单宽度

--- src/theme/settings.ts
+++ src/theme/settings.ts
@@ -44,7 +44,7 @@ export const themeSettings: App.Theme.ThemeSetting = {
   fixedHeaderAndTab: true,
   sider: {
     inverted: false,
-    width: 220,
+    width: 180,
     collapsedWidth: 64,
     mixWidth: 90,
     mixCollapsedWidth: 64,
